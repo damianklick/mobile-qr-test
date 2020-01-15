a26d7d429a7579479839669163519b6c090dd892
Damian Ruiz <druiz@klick.com>
2020-01-15 18:08:49 -0500

    main functionality done

---
+++ script.js
@@ -0,0 +1,11 @@
+const generate = document.getElementById("generate");
+const urlText = document.getElementById("urlText");
+const qrCode = document.getElementById("qrCode");
+
+generate.addEventListener("click", e => {
+    e.preventDefault();
+    let url = urlText.value;
+    console.log(url);
+    let qrUrl = `https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${url}&choe=UTF-8`
+    qrCode.setAttribute("src", qrUrl);
+})
