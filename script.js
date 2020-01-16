29f21dd5bf9b1565211fc63ddc27b2620a5be98e
Damian Ruiz <druiz@klick.com>
2020-01-16 09:47:58 -0500

    generate on enter key added

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ generate.addEventListener("click", e => {
     let qrUrl = `https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${url}&choe=UTF-8`
     qrCode.setAttribute("src", qrUrl);
 })
+
+urlText.addEventListener("keyup", e => {
+    if (e.keyCode === 13) {
+        generate.click();
+    }
+})
