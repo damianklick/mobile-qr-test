4e3fa30418cd8f70ee106071e02d0f4ab9802ca0
Damian Ruiz <druiz@klick.com>
2021-03-01 10:13:18 -0500

    fallback api added

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const generate = document.getElementById("generate");
 const urlText = document.getElementById("urlText");
-const qrCode = document.getElementById("qrCode");
+const qrCodeGoogle = document.getElementById("qrCodeGoogle");
+const qrCodeGoQR = document.getElementById("qrCodeGoQR");
 
 generate.addEventListener("click", e => {
     e.preventDefault();
     let url = urlText.value;
     console.log(url);
-    let qrUrl = `https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${url}&choe=UTF-8`
-    qrCode.setAttribute("src", qrUrl);
+    let qrUrlGoogle = `https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${url}&choe=UTF-8`
+    qrCodeGoogle.setAttribute("src", qrUrlGoogle);
+    let qrUrlGoQR = ` https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${url}`
+    qrCodeGoQR.setAttribute("src", qrUrlGoQR);
 })
 
 urlText.addEventListener("keyup", e => {
